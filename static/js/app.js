2a5c88a9a003e58c15cc38fabdd8fbfb77ad4850
ellegenzi <106830513+ellegenzi@users.noreply.github.com>
2022-09-16 23:02:07 -0400

    Adding updated app.js file.

--- static/js/app.js
+++ static/js/app.js
@@ -27,10 +27,35 @@ function buildTable(data) {
             // Add the values
             cell.text(val);
 
-
         });
     });
+}
 
+// Create a new function, handleClick
+function handleClick() {
+    // Create a couple of variables to hold our date data, both filtered and unfiltered
+    let date = d3.select("#datetime").property("value");
 
+    // Set a default filter and save it to a new variable
+    let filteredData = tableData;
+
+    // Check for a date filter using an if statement
+    // "If there is a date already set, then use that date as a filter. If not, then return the default data."
+    if (date) {
+        // "Show only the rows where the date is equal to the date filter we created above."
+        // The triple equal signs test for strict equality, meaning that the date in the table has to match our filter exactly.
+        // A double equal sign is checking for loose equality - both type and value are loosely matched.
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // Note: If no date was entered, then filteredData will just be the original tableData.
+    buildTable(filteredData);
 
 }
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
