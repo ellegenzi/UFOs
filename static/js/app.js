269f5568f982e19c0e3591fdeee96cbb8e3ad8d6
ellegenzi <106830513+ellegenzi@users.noreply.github.com>
2022-09-19 00:10:57 -0400

    Adding final challenge files.

--- static/js/app.js
+++ static/js/app.js
@@ -71,19 +71,19 @@ function updateFilters() {
     };
 
     if (city) {
-      filteredData = filteredData.filter(row => row.datetime === city);
+      filteredData = filteredData.filter(row => row.city === city);
     };
 
     if (state) {
-      filteredData = filteredData.filter(row => row.datetime === state);
+      filteredData = filteredData.filter(row => row.state === state);
     };
 
     if (country) {
-      filteredData = filteredData.filter(row => row.datetime === country);
+      filteredData = filteredData.filter(row => row.country === country);
     };
 
     if (shape) {
-      filteredData = filteredData.filter(row => row.datetime === shape);
+      filteredData = filteredData.filter(row => row.shape === shape);
     };
   
     // 10. Finally, rebuild the table using the filtered data
